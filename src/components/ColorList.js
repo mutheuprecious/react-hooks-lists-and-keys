4bb808452a8af61cedd4e9d0ace083989c3e3c0f
Precious <preciousmaregwa1997@gmail.com>
2022-05-03 01:28:52 +0300

    complete assigment

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -8,19 +8,19 @@ function ColorList() {
     "darkslategray",
     "hotpink",
   ];
+  const colorElements = colors.map((element)=>{
+    return <li key={element} style={{color:element}}>{element}</li>
+  })
 
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
+        {colorElements}
       </ol>
     </div>
   );
 }
 
 export default ColorList;
+  
